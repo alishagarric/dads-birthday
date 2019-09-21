739594d73d295948987bda4c742d0521e6e2d9dc
Alisha <alishagarric@hotmail.com>
2019-09-20 19:47:16 -0700

    :tada: initial commit: with some styling progress

---
+++ postcss.config.js
@@ -0,0 +1,26 @@
+let config = require('./config');
+
+// See: https://github.com/postcss/postcss-loader#usage
+let postcssConfig = {
+  plugins: [
+    require('autoprefixer')({
+      browsers: [
+        // For more browsers, see https://github.com/ai/browserslist
+        '> .1%',
+        'last 2 versions'
+      ]
+    }),
+    require('stylefmt')
+  ]
+};
+
+if (config.production) {
+  postcssConfig.plugins.push(
+    require('postcss-csso')({
+      restructure: false,
+      debug: true
+    })
+  );
+}
+
+module.exports = postcssConfig;
